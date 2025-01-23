b7b69157de4a01efb07c2f6d1a889f2b737f97cf
Tohid Hanfi <119034342+tohidhanfi20@users.noreply.github.com>
2025-01-23 11:41:28 +0530

    Create HealthController.java

---
+++ backend/src/test/java/com/coorg/sak/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.coorg.sak.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "Backend is running!";
+    }
+}
