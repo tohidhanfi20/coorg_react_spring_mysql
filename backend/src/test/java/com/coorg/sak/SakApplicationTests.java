a047f493ef58f8bbd76870f0bdddffc77cda6829
Tohid Hanfi <119034342+tohidhanfi20@users.noreply.github.com>
2025-01-23 12:08:11 +0530

    Update SakApplicationTests.java

--- backend/src/test/java/com/coorg/sak/SakApplicationTests.java
+++ backend/src/test/java/com/coorg/sak/SakApplicationTests.java
@@ -1,13 +1,26 @@
 package com.coorg.sak;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
-class SakApplicationTests {
+@AutoConfigureMockMvc
+class HealthControllerTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void healthCheckTest() throws Exception {
+        mockMvc.perform(get("/api/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Backend is running!"));
+    }
 }
